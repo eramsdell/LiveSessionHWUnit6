26492686c419b40fcdd38c3fe50c48047fdd042b
Elizabeth Ramsdell <eramsdell@smu.edu>
2016-10-21 09:22:06 -0400

    Commit 3
    Edit Data Cleaning File

--- Data/Data.R
+++ Data/Data.R
@@ -1,9 +1,12 @@
-setwd("~/Doing Data Science/LiveSessionHWUnit6")
-read.csv("rollingsales_queens.csv")
-Queens <- read.csv("rollingsales_queens.csv")
+## Authors: Beth Ramsdell, Marc Naples, Peter Byrd
 
-##Read CSV input file
-Queens <- read.csv("rollingsales_queens.csv", stringsAsFactors = FALSE)
+## Set the working directory and load packages
+setwd("\Users\bethd\Documents\GitHub\LiveSessionHWUnit6\Data")
+library(plyr)
+library(gdata)
+
+## Read CSV input file
+Queens <- read.csv("rollingsales_queens.csv", skip=4,header=TRUE)
 
 ##Check the data
 head(Queens)
@@ -22,30 +25,29 @@ Queens$gross.sqft <- as.numeric(gsub("[^[:digit:]]","", Queens$gross.square.feet
 Queens$land.sqft <- as.numeric(gsub("[^[:digit:]]","", Queens$land.square.feet))
 Queens$year.built <- as.numeric(as.character(Queens$year.built))
 str(Queens)
-attach(Queens)
 
-##exploration on sales price
+## exploration on sales price
+attach(Queens)
 hist(sale.price.n) # Something weird here
 hist(sale.price.n[sale.price.n>0])
 detach(Queens)
 
-##histograms do not look right, keep actual sales only
+## histograms do not look right, keep actual sales only
 
 Queens.sale <- Queens[Queens$sale.price.n!=0,]
 plot(Queens.sale$gross.sqft,Queens.sale$sale.price.n)
-plot(log(Queens.sale$gross.sqft),log(Queens.sale$sale.price.n))
+plot(log10(Queens.sale$gross.sqft),log10(Queens.sale$sale.price.n))
 
 ## remove outliers that seem like they weren't actual sales
-Queens.homes$outliers <- (log(Queens.homes$sale.price.n) <=5) + 0
+## Queens.homes$outliers <- (log(Queens.homes$sale.price.n) <=5) + 0 (this line is listed further down)
 
 ## for simplicity, let's look at 1-, 2-, and 3-family homes
 Queens.homes <- Queens.sale[which(grepl("FAMILY",Queens.sale$building.class.category)),]
 dim(Queens.homes)
-plot(log(Queens.homes$gross.sqft),log(Queens.homes$sale.price.n))
+plot(log10(Queens.homes$gross.sqft),log10(Queens.homes$sale.price.n))
 summary(Queens.homes[which(Queens.homes$sale.price.n<100000),])
 
 ## remove outliers that seem like they weren't actual sales
-Queens.homes$outliers <- (log(Queens.homes$sale.price.n) <=5) + 0
+Queens.homes$outliers <- (log10(Queens.homes$sale.price.n) <=5) + 0
 Queens.homes <- Queens.homes[which(Queens.homes$outliers==0),]
-plot(log(Queens.homes$gross.sqft),log(Queens.homes$sale.price.n))
-
+plot(log10(Queens.homes$gross.sqft),log10(Queens.homes$sale.price.n))
